a2a1b5054ce173251ec4eb42e7d7cb728c6172df
ashish29-tech <ashishranjan503@gmail.com>
2026-02-05 18:20:15 +0530

    lec 2

--- src/Lec_2/Pattern_2.java
+++ src/Lec_2/Pattern_2.java
@@ -9,11 +9,12 @@ public class Pattern_2 {
 		int star = n;
 		while(row<=n) {
 			int i = 1;
-			while(i<=row) {
-				System.out.print("* ");
+			while(i<=row) { //counter ko row ke barabar kar diya...
+				System.out.print("* "); // i and row dono 1 hai toh first row m 1 star ayega
 				i++;
 			}
 			//next row
+			System.out.println();
 			System.out.println();
 			row++;
 		}

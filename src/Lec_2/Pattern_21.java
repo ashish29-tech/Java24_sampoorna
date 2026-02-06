c285b02262a9186624c541423598737e9900ea62
ashish29-tech <ashishranjan503@gmail.com>
2026-02-06 19:17:15 +0530

    lec 2

---
+++ src/Lec_2/Pattern_21.java
@@ -0,0 +1,43 @@
+package Lec_2;
+
+public class Pattern_21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//maan ke chalte hai 1 star jyada hai last row m toh isse dono traf barabar star honge
+		//consider this pattern as a rectangle and it has length and height
+		//length in terms of n... 2*n-1 and for space 2*n-3
+		int n = 5;
+		int row = 1;
+		int star = 1;
+		int space = 2*n-3;
+		while(row<=n) {
+			//star
+			int i = 1;
+			while(i<=star) {
+				System.out.print("* ");
+				i++;
+			}
+			//space
+			int j =1;
+			while(j<=space) {
+				System.out.print("  ");
+				j++;
+			}
+			//star
+			int k = 1;
+			if(row == n) {
+				k=2;
+			}
+			while(k<=star) {
+				System.out.print("* ");
+				k++;
+			}
+			System.out.println();
+			row++;
+			star++;
+			space-=2; //space is getting reduced to two
+		}
+	}
+
+}
---
+++ src/Lec_2/Pattern_26.java
@@ -0,0 +1,30 @@
+package Lec_2;
+
+public class Pattern_26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int row = 1;
+		int star = 1;
+		int space = n - 1;
+		while (row <= n) {
+			int i = 1;
+			while (i <= space) {
+				System.out.print("  ");
+				i++;
+			}
+			int j = 1;
+			while (j <= star) {
+				System.out.print(row + " ");
+				j++;
+			}
+			// next row prep
+			System.out.println();
+			row++;
+			space--;
+			star += 2;
+		}
+	}
+
+}

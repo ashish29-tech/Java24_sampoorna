d21a287f8b44c784c792bc2761720209beba341d
ashish29-tech <ashishranjan503@gmail.com>
2026-02-09 13:57:31 +0530

    lec 2

--- src/Lec_2/Pattern_25.java
+++ src/Lec_2/Pattern_25.java
@@ -4,7 +4,30 @@ public class Pattern_25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//ek num value le lenge and use badhate rahenge jaise-jaise star inc ho rahe hai
+		int n = 5;
+		int row = 1;
+		int star = 1;
+		int space = n-1;
+		int val = 1;
+		while(row<=n) {
+			int i = 1;
+			while(i<=space) {
+				System.out.print("\t"); //jab number ho toh tab separated space
+				i++;
+			}
+			int j = 1;
+			while(j<=star) {
+				System.out.print(val + "\t");
+				val++;
+				j++;
+			}
+			//next row prep
+			System.out.println();
+			row++;
+			space--;
+			star+=2;
+		}
 	}
 
 }

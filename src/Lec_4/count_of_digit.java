01fd6a5f9b1cf827bf6b98daf0f9ac7b6f046d53
ashish29-tech <ashishranjan503@gmail.com>
2026-02-17 17:33:36 +0530

    Lec_4

---
+++ src/Lec_4/count_of_digit.java
@@ -0,0 +1,33 @@
+package Lec_4;
+
+public class count_of_digit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int n = 123456789;
+//		int count = 0;
+//		while(n>0) {
+//			n = n / 10;
+//			count++; //count karne ke liye counter le liya...c
+//		}
+//		System.out.println(count); 
+		
+		//better code cuz handles edge cases..cuz n can be 0 but still it
+		int n = 0;
+		int count = 0;
+		
+		if(n == 0) {
+			count = 1;
+//			count++;
+		}
+		else {
+			while(n>0) {
+				n = n /10;
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+	}
+
+}

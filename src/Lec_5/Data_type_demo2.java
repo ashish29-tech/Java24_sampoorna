154abaeb4df8fa20a3035984e2950b4f0994da8b
ashish29-tech <ashishranjan503@gmail.com>
2026-02-20 18:04:38 +0530

    Lec_5

--- src/Lec_5/Data_type_demo2.java
+++ src/Lec_5/Data_type_demo2.java
@@ -11,6 +11,14 @@ public class Data_type_demo2 {
 		Scanner sc = new Scanner(System.in);
 		b = sc.nextBoolean();
 		System.out.println(b);
+		//float and double
+		//double m literal ka type double hota hai
+		double d = 8989.78;
+		float f = 78.6f; //literal ka type yha double hota hai and double wale ko float m rakhenge toh error dega isliye yha bhi typecase karna padega
+		d = sc.nextDouble(); //user input
+		f = sc.nextFloat();
+		System.out.println(d);
+		System.out.println(f);
 	}
 
 }

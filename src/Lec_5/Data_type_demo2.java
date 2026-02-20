4f64271e6b325a4ffd48ce1229ac1eb001bb1572
ashish29-tech <ashishranjan503@gmail.com>
2026-02-20 17:58:12 +0530

    Lec_5

---
+++ src/Lec_5/Data_type_demo2.java
@@ -0,0 +1,16 @@
+package Lec_5;
+
+import java.util.Scanner;
+
+public class Data_type_demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b = true;
+		boolean b1 = false;
+		Scanner sc = new Scanner(System.in);
+		b = sc.nextBoolean();
+		System.out.println(b);
+	}
+
+}

3356004478895df78adab777e17287e87287deae
ashish29-tech <ashishranjan503@gmail.com>
2026-02-24 16:54:16 +0530

    Lec_6

--- src/Lec_6/Fun_Demo_3.java
+++ src/Lec_6/Fun_Demo_3.java
@@ -7,18 +7,18 @@ public class Fun_Demo_3 {
 		System.out.println("Hello");
 		int a = 8;
 		int b = 7;
-		Add(b,a); 
+		System.out.println(Add(b,a));
 		System.out.println("Bye");
 	}
 	public static int Add(int a, int b) { //a =7 and b =8
 		int c = a+b; // 7+8
-//		Sub(c,a); //15 + 7
-		return c; // method se wapis jayega toh integer jaisa value sath leke jayega
+		
+		return c + Sub(c,a); //
 	}
 	
-//	public static void Sub(int a, int b) { //a = 15, b =7
-//		int c = a-b; //15-7
-//		System.out.println(c); //8
-//	}
+	public static int Sub(int a, int b) { //a = 15, b =7
+		int c = a-b; //15-7
+		return c;
+	}
 
 }

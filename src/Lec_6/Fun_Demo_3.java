4ad63ae277b3d9a6a2b5c200df923d53a8f730f9
ashish29-tech <ashishranjan503@gmail.com>
2026-02-24 17:07:43 +0530

    Lec_6

--- src/Lec_6/Fun_Demo_3.java
+++ src/Lec_6/Fun_Demo_3.java
@@ -2,20 +2,21 @@ package Lec_6;
 
 public class Fun_Demo_3 {
 	
-	//Global variable ke sath static lagana padta hai. Ye Global variable can access any method.
+	//Ye global variable stack m nai bnata blki heap memory m banta hai. Global variable ke sath static lagana padta hai. Ye Global variable can access any method.
 	static int val = 100; //Global variable. Class ke andar but kisi bhi method ke bhr banta hai.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		int a = 8;
 		int b = 7;
+		System.out.println(val);
 		System.out.println(Add(b,a));
 		System.out.println(val);
 		System.out.println("Bye");
 	}
 	public static int Add(int a, int b) { //a =7 and b =8
 		int c = a+b; // 7+8
-		
+		val = val -5;
 		return c + Sub(c,a); //
 	}
 	

ca8773803d1cd69ff95949de637d5ecf7752886b
ashish29-tech <ashishranjan503@gmail.com>
2026-03-13 18:17:08 +0530

    Chewbacca and  number

--- src/Lec_6/Odd_Even_BackInDelhi.java
+++ src/Lec_6/Odd_Even_BackInDelhi.java
@@ -7,8 +7,16 @@ public class Odd_Even_BackInDelhi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		Odd_and_Even(n);
+		int n = sc.nextInt(); //2 yha aa jayga
+//		n no. of cars ke liye karna hai toh..
+		while(n>0) { //neeche n-- kiya toh yha 1 aya and 2nd input ke liye calulate hoga answer then dobar n-- hoga toh n 0 ho jayega and loop nahi chalega
+//			pehle 1 input ayeaga
+			int car_no = sc.nextInt(); //har baar car number input lenge.
+//			then 1 input ke liye answer calculate hoga
+			Odd_and_Even(car_no);
+			n--; //n-- kiya toh n ki value 1 ho jayegi t
+		}
+		
 		
 	}
 //	function ka return type void bna diya yahi return kar denge

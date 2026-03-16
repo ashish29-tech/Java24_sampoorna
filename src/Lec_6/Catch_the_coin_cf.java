f0f92aadf5b791069a56a1953adc1d1b7b45d5f3
ashish29-tech <ashishranjan503@gmail.com>
2026-03-16 14:50:11 +0530

    Catch the coin CF

---
+++ src/Lec_6/Catch_the_coin_cf.java
@@ -0,0 +1,10 @@
+package Lec_6;
+
+public class Catch_the_coin_cf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Lec_6/simple_input.java
@@ -0,0 +1,10 @@
+package Lec_6;
+
+public class simple_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

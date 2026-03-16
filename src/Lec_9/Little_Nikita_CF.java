26d7a435f0f11613a7888419a9f809e6a3012649
ashish29-tech <ashishranjan503@gmail.com>
2026-03-16 18:00:43 +0530

    Little Nikita CF

---
+++ src/Lec_9/Little_Nikita_CF.java
@@ -0,0 +1,10 @@
+package Lec_9;
+
+public class Little_Nikita_CF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Lec_9/Three_Brothers_CF.java
@@ -0,0 +1,22 @@
+package Lec_9;
+
+import java.util.Scanner;
+
+public class Three_Brothers_CF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	https://codeforces.com/problemset/problem/2010/B
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		ans(a,b);
+	}
+	
+	public static void ans(int a, int b) {
+		
+		int sum = a+b;
+		System.out.println(6-sum);
+	}
+
+}

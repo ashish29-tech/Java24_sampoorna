c9a47451c4c56ed50fc23cb45f1b819f72d57f9c
ashish29-tech <ashishranjan503@gmail.com>
2026-03-16 15:19:52 +0530

    simple_input

--- src/Lec_6/Catch_the_coin_cf.java
+++ src/Lec_6/Catch_the_coin_cf.java
@@ -1,10 +1,24 @@
 package Lec_6;
 
+import java.util.Scanner;
+
 public class Catch_the_coin_cf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+//		multiple coordinates/test cases ke liye
+		int n = sc.nextInt();
+		while(n>0) {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(y<=-2) {
+			System.out.println("NO");
+		}
+		else {
+			System.out.println("YES");
+		}
+		n--;
 	}
-
+	}
 }

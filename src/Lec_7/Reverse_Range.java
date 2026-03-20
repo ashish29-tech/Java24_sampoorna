1acd08ff89677ecc08e7d56f6f3ade2aa45f9015
ashish29-tech <ashishranjan503@gmail.com>
2026-03-20 18:50:37 +0530

    updates

--- src/Lec_7/Reverse_Range.java
+++ src/Lec_7/Reverse_Range.java
@@ -4,8 +4,8 @@ public class Reverse_Range {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] arr = {3, 5, 1, 7, 8, 6, 9, 11, 15, 17, 18, 16, 23, 27}; //7 to 16... i.e 3rd index se 11th index tak range reverse
-		
+//		int [] arr = {3, 5, 1, 7, 8, 6, 9, 11, 15, 17, 18, 16, 23, 27}; //7 to 16... i.e 3rd index se 11th index tak range reverse
+		int [] arr = {3, 5, 45, 42, 11, 23, 27, 8, 6, 9, 11, 15, 17, 18, 16};
 		Reverse(arr, 3, 11); //3rd index se 11th index tak range reverse karna hai
 		for(int i = 0; i<arr.length; i++) {
 			System.out.print(arr[i] + " ");

928a5fdab37c63e0af96f2f92852221cef751a59
ashish29-tech <ashishranjan503@gmail.com>
2026-03-30 15:12:32 +0530

    Product of array except self

--- src/Lec_8/Product_of_Array_Except_Self.java
+++ src/Lec_8/Product_of_Array_Except_Self.java
@@ -4,7 +4,37 @@ public class Product_of_Array_Except_Self {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] arr = {1,2,3,4};
+//		System.out.println(Product(arr)); //ye address print karega
+//		return type array hai
+		int[] a = Product(arr); //is address ko capture kar liye a variable m
+//		now address pe loop laga diya
+		for(int i = 0; i<a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+//	array return karna hai...integer ka 1D array return kar rahe hai
+	public static int [] Product(int [] arr) {
+		int n = arr.length; // arr ka length nikal liya
+		int [] left = new int[n];
+		int [] right = new int[n];
+		left[0] = 1; //left ke 0th index pe 1 rakh diya
+//		left build kar rahe
+		for(int i = 1; i<n; i++) {
+			left[i] = left[i-1] * arr[i-1]; //
+		}
+		right[n-1] = 1;
+//		n-2 se chalega loop
+		for(int i = n-2; i>=0; i--) {
+			right[i] = right [i+1] * arr[i+1];
+		}
+//		left and right build ho gya now kisi bhi left, right, ya main array m update kar sakte hai
+//		Here we are updating in left array
+		for(int i = 0; i<n; i++) {
+			left[i] = left[i] * right[i];
+			
+		}
+		return left; //left array return karna hai cuz left m changes kiya hai hamne
 	}
 
 }
---
+++ src/Lec_9/Bubble_Sort.java
@@ -0,0 +1,28 @@
+package Lec_9;
+
+public class Bubble_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = {4,5,3,2,1};
+		sort(arr);
+//		array display 
+		for(int i =0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	public static void sort(int [] arr) {
+//		ek var le lete hai jo ki n-1 times chalega
+		for(int turn =1; turn < arr.length ; turn++) {
+//			har turn m kya kaam karna hai
+			for(int i= 0; i< arr.length-turn ; i++) { //array length 5 hai toh 3 baar chal rha first time..0 se 3 tak
+				if(arr[i]>arr[i+1]) { //current element aage wale element se bada ho toh swap
+					int temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				}
+			}
+		}
+	}
+
+}

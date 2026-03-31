a9e39f5bda2fcbd489c4b8cf180b11bcab3793a9
ashish29-tech <ashishranjan503@gmail.com>
2026-03-31 15:58:17 +0530

    Maximum_Subarray_TLE

---
+++ src/Lec_9/Maximum_Subarray.java
@@ -0,0 +1,24 @@
+package Lec_9;
+
+public class Maximum_Subarray {
+// https://leetcode.com/problems/maximum-subarray/description/ 
+//	Output: 6
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(MaximumSum(arr));
+	}
+	public static int MaximumSum(int [] arr) {
+		
+		int ans = Integer.MIN_VALUE;
+		for(int i = 0; i < arr.length; i++) {
+			int sum = 0; //jab jab i change hoga tab tab i ko ham 0 se start karenge
+			for(int j = i; j<arr.length; j++) {
+				sum = sum + arr[j]; //sum m add kar diya arr[j]
+				ans = Math.max(ans, sum ); //jo max hoga dono ka use ans m dal denge
+			}
+		}
+		return ans;
+	}
+
+}

347d83d7828824cc0a347e1e6d39b479b7431528
ashish29-tech <ashishranjan503@gmail.com>
2026-04-09 19:57:36 +0530

    update

--- src/Lec_9/Insertion_Sort.java
+++ src/Lec_9/Insertion_Sort.java
@@ -8,7 +8,6 @@ public class Insertion_Sort {
 //	    int idx = InsertLastElement(arr, arr.length - 1);
 	    for (int i = 0; i < arr.length; i++) {
 	        System.out.print(arr[i] + " ");
-	        System.out.print(arr[i] + " ");
 	    }
 //	    System.out.println(idx);
 	}

5400185d9196b8e99e2a2c33b7c638449ccce430
ashish29-tech <ashishranjan503@gmail.com>
2026-04-15 15:55:18 +0530

    Time and Space Complexity asymptotic notation

--- src/Lec_11/Time_Space_Complexity.java
+++ src/Lec_11/Time_Space_Complexity.java
@@ -6,16 +6,32 @@ public class Time_Space_Complexity {
 		// TODO Auto-generated method stub
 //		There are 2 ways to calculate Time complexity
 //		Asymptotic notation and Experimental
-//		This is experimental way.
+//		This is experimental way.Output depends upon hardware.
+//		Experimental is not the way which we should be using. Bcz it's dependent on hardware.
 //		jab program run hoga tab system ka time kya hai millisecond m...wo time yaad kar liya
 //		Iska return type long hota hai...
-		long start = System.currentTimeMillis(); //loop ke chalne se pehle...time note kar lemge system ka
-		for(int i = 0; i<100000; i++) { //ye loop 10^5 times chalega
-			
-		}
-//		jab loop khtm hoga tab system ka time kya hai millisecond m yaad kar liya
-		long end = System.currentTimeMillis();
-		System.out.println(end-start); //difference aa jayega...ki loop ko execute hone m kitn time laga
+//		long start = System.currentTimeMillis(); //loop ke chalne se pehle...time note kar lemge system ka
+//		for(int i = 0; i<100000; i++) { //ye loop 10^5 times chalega
+//			
+//		}
+////		jab loop khtm hoga tab system ka time kya hai millisecond m yaad kar liya
+//		long end = System.currentTimeMillis();
+//		System.out.println(end-start); //difference aa jayega...ki loop ko execute hone m kitn time laga
+		
+		
+//		asymptotic notation
+		System.out.println("Hello"); //Ek statement ko print karne m kitna time lagega ? Constant time lagega..
+//		chahe 5 baar kiya tab bhi constant time...
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+//		Jitne bhi arithmetic, comparison, logical operator...in sabme constant time
+//		Each statement = O(1)
+//		Printing one line → O(1)
+//		Arithmetic (+, -, *, /) → O(1)
+//		Comparison (>, <, ==) → O(1)
+//		Constant time means: Execution does NOT grow with input size n
 	}
 
 }

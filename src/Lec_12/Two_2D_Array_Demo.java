23aa08b22205d5f157892e634e148afc4f15edb9
ashish29-tech <ashishranjan503@gmail.com>
2026-05-05 15:03:21 +0530

    2D array

--- src/Lec_12/Two_2D_Array_Demo.java
+++ src/Lec_12/Two_2D_Array_Demo.java
@@ -6,9 +6,9 @@ public class Two_2D_Array_Demo {
 		// TODO Auto-generated method stub
 		
 		int [][] arr = new int [3][4]; //define karna padega kitne row and column wala bnau. 3 row and 4 column
-		System.out.println(arr[0]);
-		System.out.println(arr);
-		
+		System.out.println(arr[0]); //ye 1D array ayega
+		System.out.println(arr); //ye 2D array ayega
+		System.out.println(arr[0][2]); //0th row 2 column ki value print hogi
 	}
 
 }

0764bf490eff2a65d3097f7a2f8644402bf34167
ashish29-tech <ashishranjan503@gmail.com>
2026-05-05 15:02:58 +0530

    2D array

---
+++ src/Lec_12/Two_2D_Array_Demo.java
@@ -0,0 +1,14 @@
+package Lec_12;
+
+public class Two_2D_Array_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [][] arr = new int [3][4]; //define karna padega kitne row and column wala bnau. 3 row and 4 column
+		System.out.println(arr[0]);
+		System.out.println(arr);
+		
+	}
+
+}

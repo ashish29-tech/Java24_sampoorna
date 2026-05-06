e2ebae40eb7855f7f8d9f35e295f0b72ea7b4613
ashish29-tech <ashishranjan503@gmail.com>
2026-05-06 21:12:43 +0530

    2D array

---
+++ src/Lec_12/Wave_Print_Two_2D_Array_HardCode.java
@@ -0,0 +1,16 @@
+package Lec_12;
+
+public class Wave_Print_Two_2D_Array_HardCode {
+
+	public static void main(String[] args) {
+//		ctrl + shift + f to align...
+		// TODO Auto-generated method stub
+//		jaise 1D array m hard code kar ke array bnate the waise hi 2D array m karenge
+		int[][] arr = { { 2, 3, 4, 6 }, { 1, 5, 7, 8 }, { 11, 12, 13, 16 } };
+//		int[][] arr = { { 2, 3, 4, 6 }, { 1, 5 }, { 11, 12, 16 } }; //yha har row m column ka size alag-alag hota hai use jagged array kehte hai..
+	}
+	
+	public static void print(int [][] arr) {
+		
+	}
+}

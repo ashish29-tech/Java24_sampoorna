9de6eb5ec2597888e8c2518ff44877237bb4a1b6
ashish29-tech <ashishranjan503@gmail.com>
2026-05-07 14:35:42 +0530

    Wave_Print_Two_2D_Array_HardCode

--- src/Lec_12/Wave_Print_Two_2D_Array_HardCode.java
+++ src/Lec_12/Wave_Print_Two_2D_Array_HardCode.java
@@ -6,11 +6,26 @@ public class Wave_Print_Two_2D_Array_HardCode {
 //		ctrl + shift + f to align...
 		// TODO Auto-generated method stub
 //		jaise 1D array m hard code kar ke array bnate the waise hi 2D array m karenge
-		int[][] arr = { { 2, 3, 4, 6 }, { 1, 5, 7, 8 }, { 11, 12, 13, 16 } };
+		int[][] arr = { { 2, 3, 4, 6 }, { 1, 5, 3, 4 }, { 11, 12, 13, 16 } };
 //		int[][] arr = { { 2, 3, 4, 6 }, { 1, 5 }, { 11, 12, 16 } }; //yha har row m column ka size alag-alag hota hai use jagged array kehte hai..
+		
+		print(arr);
 	}
 	
 	public static void print(int [][] arr) {
-		
+		for(int col = 0; col < arr[0].length; col++) {
+//			if column even hai
+			if(col%2 == 0) {
+				for(int row = 0; row<arr.length; row++) {
+					System.out.print(arr[row][col] + " ");
+				}
+			}
+			else { // agr odd hai column toh niche se start hoga
+				for(int row = arr.length-1; row>=0; row--) {
+					System.out.print(arr[row][col] + " ");
+				}
+				
+			}
+		}
 	}
 }

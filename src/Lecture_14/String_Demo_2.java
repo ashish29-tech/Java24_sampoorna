e9d01c36ac0d9424dc27fd5f25f88103eab969f1
ashish29-tech <ashishranjan503@gmail.com>
2026-05-12 17:19:29 +0530

    String

--- src/Lecture_14/String_Demo_2.java
+++ src/Lecture_14/String_Demo_2.java
@@ -4,7 +4,16 @@ public class String_Demo_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		String s = "hello";
+		System.out.println(s + 10 + 40 + "bye"); //string m koi bhi data type(int,char,boolean,float,double,long) add kare toh use string ki trah lega...
+		System.out.println(10 + 40 + s + "bye"); //pehle int + int hoga
+		System.out.println(s + (10+40) + "bye"); //evaluate hoga bracket
+//		string ki length
+//		yha .length ke sath() laga hai toh ye method hai..
+		System.out.println(s.length()); //string ka naam .(dot) length se string ki length
+//		array m bhi length hoti hai and string m bhi toh dono m difference kya hota hai ?
+		int [] arr = new int[4];
+		System.out.println(arr.length); //yha bracket nai laga hai but string m .length() m bracket laga hai...yha array class m ek variable hai length
+	}	
 
 }

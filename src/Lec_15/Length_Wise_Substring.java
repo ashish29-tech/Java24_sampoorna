56fb293f98c63ef20e9f581131544b4a51127e3a
ashish29-tech <ashishranjan503@gmail.com>
2026-05-20 13:30:12 +0530

    Length_Wise_Substring

---
+++ src/Lec_15/Length_Wise_Substring.java
@@ -0,0 +1,19 @@
+package Lec_15;
+
+public class Length_Wise_Substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "hello";
+		PrintSubstring(s);
+	}
+	public static void PrintSubstring(String s) {
+		for(int len = 1; len<s.length(); len++) {
+			for(int j = len; j<s.length(); j++) {
+				int i = j-len;
+				System.out.println(s.substring(i,j));
+			}
+		}
+	}
+
+}

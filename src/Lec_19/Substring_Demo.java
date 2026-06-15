24d978226eb6c42aa6a09c6a8d579e869843ef26
ashish29-tech <ashishranjan503@gmail.com>
2026-06-15 19:13:22 +0530

    Substring_Demo

---
+++ src/Lec_19/Substring_Demo.java
@@ -0,0 +1,13 @@
+package Lec_19;
+
+public class Substring_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "codingblocks";
+		System.out.println(s.substring(2)); //index 2 se entire string print ho jayega...
+//		4th index print nahi hoga
+		System.out.println(s.substring(1,4)); //1st index se leke 3rd index tak ka content print hoga
+	}
+
+}

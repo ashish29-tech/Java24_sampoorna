bb1ac85fc6a4fb046f66caee562e4e7537088106
ashish29-tech <ashishranjan503@gmail.com>
2026-08-28 23:02:50 +0530

    update

--- src/Lec_18/Print_Inc.java
+++ src/Lec_18/Print_Inc.java
@@ -8,13 +8,15 @@ public class Print_Inc {
 		PI(n);
 		
 	}
+//	
 	public static void PI(int n) {
 		if(n==0) {
 			return ;
 		}
 		
 //		Ye head recursion hai cuz last statement recursive call ke baad bhi niche kaam ho rha hai
-		PI(n-1);
+//		Agar mujhe 1...5 print karna hai, toh pehle 1...4 print karwa leta hoon, phir 5 print kar dunga.
+		PI(n-1); //recursive call
 		System.out.println(n); 
 	}
 

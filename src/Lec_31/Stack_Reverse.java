5dec471c1b7646060afd4dde256cbcb0b2d4fef4
ashish29-tech <ashishranjan503@gmail.com>
2026-09-14 16:28:52 +0530

    Lec_30 stack_reverse

---
+++ src/Lec_31/Stack_Reverse.java
@@ -0,0 +1,41 @@
+package Lec_31;
+import java.util.*;
+
+public class Stack_Reverse {
+// Stack m kuch element given hai hame unhe reverse karna hai...
+// Hame reverse karna hai original stack m...original stack ko reverse karna hai
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		st.push(40);
+		st.push(50); //50 sabse top pe hai
+		System.out.println(st);
+		Reverse(st);
+		System.out.println(st);
+	}
+	
+	public static void Reverse(Stack<Integer> st) {
+//		Base case
+		if(st.isEmpty()) {
+			return;
+		}
+		int x = st.pop(); //x
+		Reverse(st); //stack ke top element ko ek-ek kar ke nikalta hai
+//		Jo element nikala tha, usko stack ke bilkul bottom mein insert karta hai.
+//		x ko reversed stack ke BOTTOM mein daalo
+		Insert(st, x); //x ko insert karna hai
+	}
+//	Yha insert ka code copy-paste kar diya
+	public static void Insert(Stack<Integer> st, int item) {
+		if(st.isEmpty()) {
+			st.push(item);
+			return;
+		}
+		int x = st.pop();
+		Insert(st, item);
+		st.push(x);
+	}
+}
